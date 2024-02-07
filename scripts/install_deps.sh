4c39b6b82b23e79062a27a3306de56f252c97931
Levon <lgevorgyan03@gmail.com>
2024-02-07 16:47:33 -0600

    apt

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# test for nala installation
-which nala && PKG_MNG="nala" || PKG_MNG="apt-get"
+PKG_MNG="apt-get"
 
 sudo "$PKG_MNG" update
 
@@ -9,7 +8,7 @@ sudo "$PKG_MNG" install autoconf automake build-essential cmake git-core \
   libass-dev libfreetype6-dev libsdl2-dev libtool libva-dev libvdpau-dev \
   libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev pkg-config \
   texinfo wget zlib1g-dev nasm yasm libssl-dev time python3 meson ninja-build\
-  doxygen xxd jq lshw gnuplot python3-pip -y || exit 1
+  doxygen xxd jq lshw gnuplot python3-pip curl -y || exit 1
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 source "$HOME/.cargo/env"
