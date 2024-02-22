570f7304be38a5eaa3fce1fc061bef51c366c0d9
Levon <lgevorgyan03@gmail.com>
2024-02-21 19:12:22 -0600

    make clean

--- scripts/build.sh
+++ scripts/build.sh
@@ -60,6 +60,7 @@ cd "$AOM_DIR/" || exit
 git pull
 mkdir build
 cd build || exit
+make clean
 cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON \
           -DCMAKE_C_FLAGS="-flto -O2 $EXTRA_C_FLAGS" || exit
 make -j "$(nproc)" || exit
