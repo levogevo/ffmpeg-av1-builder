c091691376ae0ff726b2fd4b415768166447d02c
Levon <lgevorgyan03@gmail.com>
2024-03-16 11:55:25 -0500

    gcc-12

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -6,7 +6,7 @@ sudo "$PKG_MNG" update
 sudo "$PKG_MNG" upgrade -qy
 
 sudo "$PKG_MNG" install autoconf automake build-essential cmake git-core \
-  libass-dev libfreetype6-dev libsdl2-dev libtool libva-dev libvdpau-dev \
+  libass-dev libfreetype6-dev libsdl2-dev libtool libva-dev libvdpau-dev gcc-12 \
   libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev pkg-config bc \
   texinfo wget zlib1g-dev nasm yasm libssl-dev time python3 meson ninja-build gobjc++ \
   doxygen xxd jq lshw gnuplot python3-pip curl clang valgrind ccache gawk mawk -y || exit 1
