8a6d6c408e9085c71db84f5cb57757dde0dac14b
Levon <lgevorgyan03@gmail.com>
2024-03-22 13:56:06 -0500

    cleanup

--- scripts/build.sh
+++ scripts/build.sh
@@ -47,10 +47,11 @@ then
 
   # build mpp
   cd "$RKMPP_DIR/" || exit
+  git stash && git stash drop
   git pull
-  rm -rf mpp_build
-  mkdir mpp_build
-  cd mpp_build || exit
+  rm -rf mpp_build.user
+  mkdir mpp_build.user
+  cd mpp_build.user || exit
   make clean
   cmake .. -DCMAKE_BUILD_TYPE=Release \
            -DBUILD_SHARED_LIBS=ON \
@@ -62,15 +63,16 @@ then
 
   # build rga
   cd "$RKRGA_DIR" || exit
+  git stash && git stash drop
   git pull
-  rm -rf rga_build
-  mkdir rga_build
-  cd rga_build || exit
-  meson setup ../ rga_build --buildtype release -Db_lto=true \
+  rm -rf rga_build.user
+  mkdir rga_build.user
+  cd rga_build.user || exit
+  meson setup ../ rga_build.user --buildtype release -Db_lto=true \
      --default-library=shared -Dlibdrm=false -Dlibrga_demo=false \
      --optimization=3 -Dc_args="$COMP_FLAGS" -Dcpp_args="-fpermissive $COMP_FLAGS" || exit
-  ninja -vC rga_build || exit
-  sudo ninja -vC rga_build install || exit
+  ninja -vC rga_build.user || exit
+  sudo ninja -vC rga_build.user install || exit
 else
   git clone https://git.ffmpeg.org/ffmpeg.git "$FFMPEG_DIR" --depth 1  
 fi
@@ -78,9 +80,9 @@ fi
 # build svt-av1
 cd "$SVT_DIR/" || exit
 git pull
-rm -rf build_svt
-mkdir build_svt
-cd build_svt || exit
+rm -rf build_svt.user
+mkdir build_svt.user
+cd build_svt.user || exit
 make clean
 cmake .. -DCMAKE_BUILD_TYPE=Release -DSVT_AV1_LTO=ON \
           -DCMAKE_C_FLAGS="-O3 $COMP_FLAGS" \
@@ -90,23 +92,26 @@ sudo make install || exit
 
 # build rav1e
 cd "$RAV1E_DIR/" || exit
+git stash && git stash drop
 git pull
-rm -rf ffmpeg_build && mkdir ffmpeg_build || exit
+rm -rf ffmpeg_build.user && mkdir ffmpeg_build.user || exit
 source "$HOME/.cargo/env" # for good measure
 cargo clean
 RUSTFLAGS="-C target-cpu=native" cargo cinstall --release \
-     --prefix="$(pwd)"/ffmpeg_build \
-     --libdir="$(pwd)"/ffmpeg_build/lib \
-     --includedir="$(pwd)"/ffmpeg_build/include || exit
-cd ffmpeg_build || exit
+     --prefix="$(pwd)"/ffmpeg_build.user \
+     --libdir="$(pwd)"/ffmpeg_build.user/lib \
+     --includedir="$(pwd)"/ffmpeg_build.user/include || exit
+cd ffmpeg_build.user || exit
 sudo cp ./lib/* /usr/local/lib/ -r || exit
 sudo cp ./include/* /usr/local/include/ -r || exit
 
 # build aom
 cd "$AOM_DIR/" || exit
+git stash && git stash drop
 git pull
-mkdir build
-cd build || exit
+rm -rf build_aom.user
+mkdir build_aom.user
+cd build_aom.user || exit
 make clean
 cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON \
           -DENABLE_TESTS=OFF \
@@ -117,31 +122,34 @@ sudo make install || exit
 
 # build libvmaf
 cd "$VMAF_DIR/libvmaf" || exit
+git stash && git stash drop
 git pull
 python3 -m virtualenv .venv
 source .venv/bin/activate
-rm -rf build
-mkdir build
-cd build || exit
+rm -rf build.user
+mkdir build.user
+cd build.user || exit
 pip install meson
-meson setup ../ build --buildtype release -Denable_float=true -Db_lto=true \
+meson setup ../ build.user --buildtype release -Denable_float=true -Db_lto=true \
      --optimization=3 -Dc_args="$COMP_FLAGS" -Dcpp_args="$COMP_FLAGS" || exit
-ninja -vC build || exit
-sudo ninja -vC build install || exit
+ninja -vC build.user || exit
+sudo ninja -vC build.user install || exit
 
 # build dav1d
 cd "$DAV1D_DIR" || exit
+git stash && git stash drop
 git pull
-rm -rf build
-mkdir build
-cd build || exit
-meson setup ../ build --buildtype release -Db_lto=true \
+rm -rf build.user
+mkdir build.user
+cd build.user || exit
+meson setup ../ build.user --buildtype release -Db_lto=true \
      --optimization=3 -Dc_args="$COMP_FLAGS" -Dcpp_args="$COMP_FLAGS" || exit
-ninja -vC build || exit
-sudo ninja -vC build install || exit
+ninja -vC build.user || exit
+sudo ninja -vC build.user install || exit
 
 # build opus
 cd "$OPUS_DIR" || exit
+git stash && git stash drop
 git pull
 ./autogen.sh || exit
 export CFLAGS="-O3 -flto $COMP_FLAGS"
@@ -157,6 +165,7 @@ sudo ldconfig
 
 # build ffmpeg
 cd "$FFMPEG_DIR/" || exit
+git stash && git stash drop
 git pull
 export PKG_CONFIG_PATH+=":/usr/local/lib/pkgconfig"
 make clean
